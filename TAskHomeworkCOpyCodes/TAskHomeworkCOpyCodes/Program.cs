a8694de711bb3c90b9839918e20621b1df8f2fc4
MahammadIsgenderov <isgenderovmahammad4@gmail.com>
2022-10-05 17:03:13 +0400

    Init

---
+++ TAskHomeworkCOpyCodes/TAskHomeworkCOpyCodes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TAskHomeworkCOpyCodes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {15,16,25,41,75,84};
+            EdediOrta(array);
+        }
+       //static void Hello(string soz,int a)
+       // {
+       //     Console.WriteLine(soz+" "+a+" "+"yashindadir");
+       // }
+       static void EdediOrta(int[] coxluq)
+        {
+            int cem = 0;
+            foreach (var item in coxluq)
+            {
+                cem=cem+item;
+             }
+            double ededlerincemi=cem;
+            Console.WriteLine(cem);
+            double b = ededlerincemi / coxluq.Length;
+            Console.WriteLine(b);
+        }
+    }
+}
